57b8d81c2bdd25abddf2c7e1d78bb728670d5b4a
Rafael Audy Glanzner <rafael.audy@gmail.com>
2019-06-01 14:55:54 +0545

    Adds header component

---
+++ ui/src/components/layout/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Header.css";
+
+class Header extends Component {
+  render() {
+    return <header />;
+  }
+}
+
+export default Header;
--- ui/src/components/layout/Layout.js
+++ ui/src/components/layout/Layout.js
@@ -1,23 +1,12 @@
 import React, { Component } from "react";
+import Header from "./Header";
 import "./Layout.css";
 
 class Layout extends Component {
   render() {
     return (
-      <div className="Layout">
-        <header className="Layout-header">
-          <p>
-            Edit <code>src/Layout.js</code> and save to reload.
-          </p>
-          <a
-            className="Layout-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header />
       </div>
     );
   }
